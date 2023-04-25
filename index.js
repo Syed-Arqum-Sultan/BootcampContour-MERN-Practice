f974978e76d976c77d5a74a0aa3c06eabd6ef52e
Syed-Arqum-Sultan <110258159+Syed-Arqum-Sultan@users.noreply.github.com>
2023-04-25 13:30:34 +0500

    Callbacks Addition

--- index.js
+++ index.js
@@ -57,3 +57,20 @@ console.log(person.bio())
    }
  }
  person2.bio()
+
+
+ //callbacks (below example shows working without callbacks!)
+
+ function myDisplayer(some) {
+    console.log(some);
+}
+
+function myCalculator(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+let result = myCalculator(5, 5);
+myDisplayer(result);
+
+///same action using callbacks!
